698416c2857b015240c9dc0420f70fd6608530bd
Fernanda Andrade <flandrade@users.noreply.github.com>
2023-01-15 06:16:13 +0100

    add substitution

--- src/LambdaCalculus/LambdaCalculus.hs
+++ src/LambdaCalculus/LambdaCalculus.hs
@@ -16,13 +16,71 @@ data LambdaTerm
   = Variable String
   | Application LambdaTerm LambdaTerm
   | Lambda String LambdaTerm
+  deriving (Show, Eq)
 
 -- Exercise 17. Based on the previous exercise, define a Haskell function that obtains the free variables
--- This function returns the set of free variables of a lambda term
-freeVariables :: LambdaTerm -> S.Set String
+-- This function returns the set of free variables of a lambda LambdaTerm
+freeVariables ::
+  -- Lambda term to perform the operation
+  LambdaTerm ->
+  -- returns the set of free variables of a lambda LambdaTerm
+  S.Set String
 freeVariables (Variable var) = S.singleton var
 freeVariables (Application lt1 lt2) = S.union (freeVariables lt1) (freeVariables lt2)
 freeVariables (Lambda var lt) = S.delete var $ freeVariables lt
 
 -- Exercise 18. Based on the previous exercises, define a Haskell function that implements capture
 -- avoiding substitution.
+-- Function to perform capture-avoiding substitution on a lambda term.
+-- The function uses recursion to traverse the lambda term, checking the variable name at each level of the term,
+-- and replacing it with the provided term if the variable name matches the variable being replaced. If the variable
+-- is bound in a lambda abstraction, the function checks if it is captured by the replacement term, and if so, it creates
+-- a fresh variable with a new name and perform the substitution on the modified term
+substitute ::
+  -- the lambda term to perform the substitution on
+  LambdaTerm ->
+  -- the variable name to be replaced
+  String ->
+  -- term that will be used as a replacement
+  LambdaTerm ->
+  -- returns the resulting term after the substitution
+  LambdaTerm
+-- if the current term is a variable and its name matches the variable to be replaced,
+-- return the replacement term
+substitute (Variable x) y sub = if x == y then sub else Variable x
+-- if the current term is an application, recursively perform substitution on both subterms
+substitute (Application t1 t2) y sub = Application (substitute t1 y sub) (substitute t2 y sub)
+-- if the current term is a lambda abstraction:
+substitute (Lambda x t1) y sub =
+  -- if the variable bound in this lambda abstraction is the variable to be replaced,
+  -- return the same lambda abstraction
+  if x == y
+    then Lambda x (substitute t1 y sub)
+    else -- otherwise, check if the bound variable is captured by the replacement term
+
+      if x `S.member` fvT2
+        then -- if it is, create a fresh variable with a new name and perform the substitution on the modified term
+
+          let x' = freshName (S.union fvT1 (S.union fvT2 (S.singleton x)))
+              t1' = substitute t1 x (Variable x')
+           in Lambda x' (substitute t1' y sub)
+        else -- otherwise, perform the substitution on the term as normal
+          Lambda x (substitute t1 y sub)
+  where
+    -- find the free variables in the current term and the replacement term
+    fvT1 = freeVariables t1
+    fvT2 = freeVariables sub
+
+-- function to generate a fresh variable name that is not already in the set of free variables
+freshName :: S.Set String -> String
+freshName s = head $ filter (\x -> not (x `S.member` s)) $ map (\i -> "v" ++ show i) [(1 :: Integer) ..]
+
+-- Exercise 19. Based on the previous exercises, define a Haskell function that implements β-reduction
+-- (one step).
+betaReduction :: Term -> Term
+betaReduction (Application (Lambda v t) e) = substitute v e t
+betaReduction (Application (Application m n') n) = Application $ (betaReduction $ Application $ m n') n
+betaReduction x = x
+
+-- Exercise 20. Based on the previous exercises, define a Haskell function that reduces a lambda LambdaTerm
+-- into β-normal form when possible.
--- test/LambdaCalculus/LambdaCalculusSpec.hs
+++ test/LambdaCalculus/LambdaCalculusSpec.hs
@@ -15,3 +15,24 @@ spec = do
       freeVariables (Lambda "y" (Application (Variable "y") (Variable "x"))) `shouldBe` S.singleton "x"
     it "returns the free variable of a lambda term - abstraction" $ do
       freeVariables (Lambda "y" (Variable "y")) `shouldBe` S.empty
+
+  describe "substitute" $ do
+    it "replaces a variable in a term" $ do
+      let t = Variable "x"
+      let e = Variable "y"
+      substitute t "x" e `shouldBe` e
+
+    it "replaces a variable in a nested term" $ do
+      let t = Lambda "x" (Variable "x")
+      let e = Variable "y"
+      substitute t "x" e `shouldBe` Lambda "x" e
+
+    it "replaces a variable in a complex term" $ do
+      let t = Application (Lambda "x" (Variable "x")) (Variable "y")
+      let e = Variable "z"
+      substitute t "y" e `shouldBe` (Application (Lambda "x" (Variable "x")) e)
+
+    it "avoids variable capture" $ do
+      let t = Lambda "x" (Application (Variable "x") (Variable "y"))
+      let e = Variable "x"
+      substitute t "y" e `shouldBe` (Lambda "v1" (Application (Variable "v1") (Variable "x")))
